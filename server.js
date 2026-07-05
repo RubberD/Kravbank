ad622bfa4958e60a39dab3b712ffeba1ccae9bac
Lasse Storgaard <lasse.storgaard@gmail.com>
2026-07-05 15:00:08 +0200

    Satte en content security policy i Node med Helmet

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Bruk Helmet for å sette sikkerhetsrelaterte HTTP-headers
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://unpkg.com"],
+    scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", "https://unpkg.com"],
+  },
+}));
 
 // Definer regler for rate limiting
 const limiter = rateLimit({
